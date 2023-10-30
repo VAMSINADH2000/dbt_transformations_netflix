75d83730f947315a1d26b5727d7eb2c847285f6e
vamsinadh2000 <vamsinadh2000@gmail.com>
2023-10-30 17:09:24 -0400

    First commit bash script4

--- run_script.sh
+++ run_script.sh
@@ -1,6 +1,5 @@
-
-# !/bin/bash
-dbt deps
-dbt run-operation stage_external_sources
-dbt run
-echo "Successfully Finished"
+#!/bin/bash
+dbt deps &> /usr/app/dbt/log.txt
+dbt run-operation stage_external_sources &>> /usr/app/dbt/log.txt
+dbt run &>> /usr/app/dbt/log.txt
+echo "Successfully Finished" &>> /usr/app/dbt/log.txt
